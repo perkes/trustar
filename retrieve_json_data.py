13fe7fdd21c652bc39fa3eb6b5d0ec6324531a68
Jonathan Perkes <jperkes@dev.azure.com>
2020-11-06 12:11:48 -0300

    config.json

--- retrieve_json_data.py
+++ retrieve_json_data.py
@@ -1,26 +1,38 @@
 from github import Github
 from src.utils import f
 
+import argparse
 import json
 import csv
 
-with open('config.json', encoding='utf-8') as config_file:
-    config = json.load(config_file) 
+def retrieve_json_data():
+    parser = argparse.ArgumentParser(description = 'Retrieve specific keys from JSON files in public Github repositories.')
+    parser.add_argument('--config', default = './config.json', help = 'Path to the config file')
+    args = parser.parse_args()
     
-    output_file = config['output_file']
-    keys = config['keys']
-    
-    if output_file:
-        writer = csv.DictWriter(open(output_file, 'w'), keys)
-        writer.writeheader()
+    with open(args.config, encoding='utf-8') as config_file:
+        config = json.load(config_file) 
         
-    g = Github(config['github_token'])
-    repo = g.get_repo(config['repo_id'])
-    files = repo.get_contents(config['file_directory'], ref = 'master')
-        
-    for file in files:
-        data = file.decoded_content.decode('utf-8')
-        ret = f(data, keys)
-        print(ret)
+        # Optional csv output file, might be blank.
+        output_file = config['output_file']
+        output_console = config['output_console']
+        keys = config['keys']
+
         if output_file:
-            writer.writerow(ret)
+            writer = csv.DictWriter(open(output_file, 'w'), keys)
+            writer.writeheader()
+
+        g = Github(config['github_token'])
+        repo = g.get_repo(config['repo_id'])
+        files = repo.get_contents(config['file_directory'], ref = 'master')
+
+        for file in files:
+            data = file.decoded_content.decode('utf-8')
+            ret = f(data, keys)
+            if output_console:
+                print(ret)
+            if output_file:
+                writer.writerow(ret)
+                
+if __name__ == "__main__":
+    retrieve_json_data()
--- src/utils.py
+++ src/utils.py
@@ -4,14 +4,52 @@ import json
 import re
 
 def get_value(key, dictionary):
+    """ Returns a value from a dictionary given a key using dot and bracket
+        notation. eg: a possible key could be 'user.posts[1].comments[2]'.
+
+    Parameters:
+    key (str)        : the key to be used to retrieve its corresponding value from
+                       the dictionary.
+    dictionary (dict): the dictionary were tha value is extracted from using the
+                       key above.
+
+    Returns:
+    int/float/str/list/dict/bool/None: Returning value
+
+    """
     try:
-        pairs = [k.replace(']', '').split('[') for k in key.split('.')]
-        keys = [int(y) if re.match(r'^\d+$', y) else y for x in pairs for y in x]
+        # We're using 2 regular expressions, the first one to try to cast the value as int if
+        # possible, the second one to convert the key using dot and bracket notation into a list
+        # of ordered keys to access the value in the dictionary.
+        keys = [int(k) if re.match(r'^\d+$', k) else k for k in re.findall(r'[^\[^\]^\.]+', key)]
+        # Reduce is here used to access any value in the dictionary using a list of keys of 
+        # arbitrary length.
         return reduce(operator.getitem, keys, dictionary)
-    except:
+    # If a particular key is invalid (which is to be expected), None is returned.
+    except (KeyError, IndexError, TypeError):
         return None
 
 def f(a,b):
+    """ Returns a dictionary containing key/value pairs. The keys are given in a
+        list (b) using dot and bracket notation (eg: user.posts[0].comments[3]),
+        the values are retrieved from the dictionary using these keys.
+
+    Parameters:
+    a (str) : the dictionary were the values will be extracted from, represented
+              as a string.
+    b (list): the list of keys (strings) to be used to retrieve the relevant
+              values from the dictionary (a) in dot and bracket notation.
+
+    Returns:
+    dict: the dictionary with the returned key/value pairs.
+
+    """
+    
+    # Since the dictionary is represented using a string, we'll
+    # load it using the built-in json library.
     dictionary = json.loads(a)
+    # For each key, we'll retrieve the corresponding value using
+    # the get_value function.
     matches = {k: get_value(k, dictionary) for k in b}
+    # We filtered Nones before returning.
     return {k: v for k, v in matches.items() if v is not None}
